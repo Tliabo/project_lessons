66900b2caaaf1e4d78520211a6ed0556175a3e96
Tobia Prezioso <tobia.prezioso@gmail.com>
2020-07-28 22:24:07 +0200

    loops & arrays

---
+++ public/module_4300/06_js_basics/04_loops/script.js
@@ -0,0 +1,17 @@
+
+// Counter
+
+for (var i = 0; i <= 10; i ++) {
+  // console.log(i)
+}
+
+var myArr = ['apple', 'Banana', 'Pear'];
+
+// console.log(myArr);
+// console.log(myArr[0]);
+// console.log(myArr[1]);
+// console.log(myArr[2]);
+
+for (var i = 0; i < 3; i++) {
+  console.log(myArr[i]);
+}
