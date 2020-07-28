e1af479af31b2dd427ef93a457ed4739a0bff07c
Tobia Prezioso <tobia.prezioso@gmail.com>
2020-07-28 17:38:15 +0200

    lessons in modules

---
+++ public/module_4300/06_js_basics/01_introduction/script.js
@@ -0,0 +1,25 @@
+/*
+  Javascript Introduction
+  (This is a block comment)
+*/
+
+// this is a line comment
+
+/*
+  Data Types
+*/
+
+// Number
+var myNumber = 24;
+
+// String
+var myText = 'Hello World';
+var strNumber = '24';
+
+// Boolean
+var foo = true;
+var bar = false;
+
+console.log(myPopcorn);
+
+var myPopcorn = 'popcornius';
