2a5d9475b8e187aab9d58f5670ca113e931e41dc
Tobia Prezioso <tobia_prezioso@proseller.ch>
2021-05-25 18:03:09 +0200

    update project

---
+++ public/module_5100/cms_abgabe/config/database.php
@@ -0,0 +1,33 @@
+<?php
+
+return [
+
+  /*
+   * Default Database Connection Name
+   *
+   * Here you may specify which database connection below you wanna use
+   */
+  'default' => 'mysql',
+
+  /*
+   * Database Connections
+   *
+   * Here are the connections that are available for the application
+   */
+  'connections' => [
+    'sqlite' => [
+      'driver' => 'sqlite',
+      'database' => 'database.sqlite'
+    ],
+
+    'mysql' => [
+      'driver' => 'mysql',
+      'host' => '127.0.0.1',
+      'port' => '3306',
+      'database' => 'vart',
+      'username' => 'homestead',
+      'password' => 'secret'
+    ]
+  ]
+
+];
