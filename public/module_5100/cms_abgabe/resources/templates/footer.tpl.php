4003d3de18a98548cd98c73d3c4bb59f9b5c8201
Tobia Prezioso <tobia.prezioso@gmail.com>
2021-06-14 09:57:10 +0200

    update styling with bootstrap

--- public/module_5100/cms_abgabe/resources/templates/footer.tpl.php
+++ public/module_5100/cms_abgabe/resources/templates/footer.tpl.php
@@ -1,11 +1,11 @@
 <footer id="footer-main" class="container-fluid">
-    <div class="d-flex justify-content-between">
-        <address>
+    <div class="d-flex justify-content-between align-items-center">
+        <address id="contact">
             <span>Contact Me:</span> <br>
             <a class="email" href="mailto:verena_prezioso@yahoo.de" target="_blank">verena_prezioso[at]yahoo.de</a><br>
             <a class="phone" href="tel:+41449204915">+41 44 920 49 15</a>
         </address>
-        <div class="text-end">
+        <div id="copyright" class="text-end">
             <span>&copy;opyright</span><br>
             <small>crafted by <a href="https://prezioso.ch" target="_blank" rel="noopener">Tobia Prezioso</a></small>
         </div>
--- public/module_5100/cms_abgabe/resources/templates/header.tpl.php
+++ public/module_5100/cms_abgabe/resources/templates/header.tpl.php
@@ -1,14 +1,14 @@
 <header id="header-main">
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <div class="container-fluid">
-            <a class="h1 navbar-brand mb-0" href="/">Verena Prezioso</a>
+            <a class="h1 navbar-brand mb-0 w-50 text-center" href="/">Verena Prezioso</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
                     data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
                     aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
+                <i class="fas fa-bars"></i>
             </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <div id="navbarSupportedContent" class="collapse navbar-collapse">
+                <ul class="navbar-nav justify-content-around w-100">
                     <li class="nav-item">
                         <a class="nav-link" href="/aktuell">Aktuell</a>
                     </li>
@@ -40,7 +40,7 @@
         </div>
     </nav>
 
-    <a id="button-toTop" href="#"><i class="fas fa-chevron-circle-up fa-3x"></i></a>
+    <a id="button-toTop" title="Back To Top" href="#"><i class="fas fa-chevron-circle-up fa-3x"></i></a>
 </header>
 
 
