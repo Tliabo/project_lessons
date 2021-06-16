06d7907274d96f55aea922b599ceefcd51de64d2
Tobia Prezioso <tobia.prezioso@gmail.com>
2021-06-16 10:29:55 +0200

    change error code handling

--- public/module_5100/cms_abgabe/database/ErrorCodeModel.php
+++ public/module_5100/cms_abgabe/database/ErrorCodeModel.php
@@ -7,7 +7,15 @@ use src\SiteModel;
 
 class ErrorCodeModel extends SiteModel
 {
-    private $code;
+    const ERROR_CODE_404 = 404;
+    const ERROR_CODE_500 = 500;
+
+    public array $errorMessages = [
+        404 => 'Page Not Found',
+        500 => 'Internal Server Error'
+    ];
+
+    private int $code;
 
     public function __construct($code)
     {
@@ -17,12 +25,22 @@ class ErrorCodeModel extends SiteModel
     public function getContend()
     {
         ob_start();
-        include_once RESOURCE_DIR . "/views/_$this->code.php";
+        include_once RESOURCE_DIR . "/views/_error.php";
         return ob_get_clean();
     }
 
     public function getTitle(): string
     {
         return "Error $this->code";
+    }
+
+    public function getErrorCode(): int
+    {
+        return $this->code;
+    }
+
+    public function getErrorMessage()
+    {
+        return $this->errorMessages[$this->getErrorCode()];
     }
 }
---
+++ public/module_5100/cms_abgabe/resources/views/_error.php
@@ -0,0 +1,2 @@
+<p class="h1 text-center"><?= $this->getErrorCode() ?></p>
+<p class="h1 text-center"><?= $this->getErrorMessage() ?></p>
