3db129504b923d182c634496779e52e282c5842d
Tobia Prezioso <tobia.prezioso@gmail.com>
2021-06-16 10:30:21 +0200

    add sql dump

---
+++ public/module_5100/cms_abgabe/database/sql_dump/15_06_2021_2020_VP-SAE_CMS.sql
@@ -0,0 +1,30 @@
+CREATE TABLE `adminUser` (
+  `id` int UNIQUE PRIMARY KEY NOT NULL,
+  `firstname` varchar(255) NOT NULL,
+  `lastname` varchar(255) NOT NULL,
+  `email` varchar(255) UNIQUE NOT NULL,
+  `password` varchar(255) NOT NULL
+);
+
+CREATE TABLE `page` (
+  `id` int UNIQUE PRIMARY KEY NOT NULL,
+  `title` varchar(255),
+  `contend` varchar(255)
+);
+
+CREATE TABLE `image` (
+  `id` int UNIQUE PRIMARY KEY NOT NULL,
+  `src` varchar(255) NOT NULL,
+  `alt` varchar(255),
+  `name` varchar(255),
+  `description` varchar(255),
+  `price` varchar(255),
+  `category` int
+);
+
+CREATE TABLE `category` (
+  `id` int UNIQUE PRIMARY KEY NOT NULL,
+  `name` varchar(255) NOT NULL
+);
+
+ALTER TABLE `image` ADD FOREIGN KEY (`category`) REFERENCES `category` (`id`);
---
+++ public/module_5100/cms_abgabe/database/sql_dump/15_06_2021_2043_VP-SAE_CMS.sql
@@ -0,0 +1,30 @@
+CREATE TABLE `adminUser` (
+  `id` int UNIQUE PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  `firstname` varchar(255) NOT NULL,
+  `lastname` varchar(255) NOT NULL,
+  `email` varchar(255) UNIQUE NOT NULL,
+  `password` varchar(255) NOT NULL
+);
+
+CREATE TABLE `page` (
+  `id` int UNIQUE PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  `title` varchar(255),
+  `contend` varchar(255)
+);
+
+CREATE TABLE `image` (
+  `id` int UNIQUE PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  `src` varchar(255) NOT NULL,
+  `alt` varchar(255),
+  `name` varchar(255),
+  `description` varchar(255),
+  `price` varchar(255),
+  `category` int
+);
+
+CREATE TABLE `category` (
+  `id` int UNIQUE PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL
+);
+
+ALTER TABLE `image` ADD FOREIGN KEY (`category`) REFERENCES `category` (`id`);
