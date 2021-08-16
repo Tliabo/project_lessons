6723740593966491a3ff2360e401551b6fc378b4
Tobia Prezioso <tobia.prezioso@gmail.com>
2021-08-16 19:47:23 +0200

    update lesson

---
+++ public/module_5100/14_php/01_kungfu/PDO/loginWithPDO/prefs/credentials.php
@@ -0,0 +1,15 @@
+<?php
+// lokale DB-Credentials
+$host = "localhost";
+$user = "rene";
+$passwd = "espo07";
+$dbname = "kungfu";
+
+// DB-Credentials Server
+/*
+$host = "";
+$user = "";
+$passwd = "";
+$dbname = "";
+*/
+?>
